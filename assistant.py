e15242b91a6b2f562d8c20f294137c2625a152f0
Nmeri Okolo <okolonmeri@gmail.com>
2024-08-11 17:05:15 +0100

    Any message

--- assistant.py
+++ assistant.py
@@ -1,61 +1,78 @@
 from typing import Optional
-
+import streamlit as st
 from phi.assistant import Assistant
 from phi.knowledge import AssistantKnowledge
-from phi.llm.openai import OpenAIChat
-from phi.tools.duckduckgo import DuckDuckGo
+from phi.llm.groq import Groq
 from phi.embedder.openai import OpenAIEmbedder
+from phi.embedder.ollama import OllamaEmbedder
 from phi.vectordb.pgvector import PgVector2
 from phi.storage.assistant.postgres import PgAssistantStorage
-from phi.embedder.ollama import OllamaEmbedder
-from phi.llm.groq import Groq
+from phi.embedder.together import TogetherEmbedder
+from phi.embedder.fireworks import FireworksEmbedder
+
+firewroks_api = "oV2Ggh7ihoe1uhfC8bCiHQSGgzbg0wGTyoAuVNqvxkFjREH7"
+
 db_url = "postgresql://neondb_owner:65WYfupDerlJ@ep-aged-dust-a5d9he9l.us-east-2.aws.neon.tech/neondb?sslmode=require"
 groq_api_key="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"
 
 
-def get_auto_rag_assistant(
+# dvs
+def get_groq_assistant(
     llm_model: str = "llama3-70b-8192",
+    embeddings_model: str = "nomic-embed-text",
     user_id: Optional[str] = None,
     run_id: Optional[str] = None,
     debug_mode: bool = True,
+    user_grade: Optional[str] = None,
 ) -> Assistant:
-    """Get an Auto RAG Assistant."""
+    """Get a Groq RAG Assistant."""
+
+    # Define the embedder based on the embeddings model
+    embedder = (
+        OllamaEmbedder(model=embeddings_model, dimensions=768)
+        if embeddings_model == "nomic-embed-text"
+        else OpenAIEmbedder(model=embeddings_model, dimensions=1536)
+    )
+    # Define the embeddings table based on the embeddings model
+    embeddings_table = (
+        "groq_rag_documents_ollama" if embeddings_model == "nomic-embed-text" else "groq_rag_documents_openai"
+    )
 
     return Assistant(
-        name="auto_rag_assistant",
+        name="groq_rag_assistant",
         run_id=run_id,
         user_id=user_id,
         llm=Groq(model="llama3-70b-8192", api_key=groq_api_key, max_tokens=6000),
-        storage=PgAssistantStorage(table_name="auto_rag_assistant_openai", db_url=db_url),
+        storage=PgAssistantStorage(table_name="groq_rag_assistant", db_url=db_url),
         knowledge_base=AssistantKnowledge(
             vector_db=PgVector2(
                 db_url=db_url,
-                collection="auto_rag_documents_openai",
-                embedder=OllamaEmbedder(model="jmorgan/nomic-embed-text", dimensions=768),
+                collection=embeddings_table,
+                embedder=FireworksEmbedder(api_key=firewroks_api),
             ),
-            # 3 references are added to the prompt
-            num_documents=3,
+            # 2 references are added to the prompt
+            num_documents=2,
         ),
-        description="You are a helpful Study Assistant called 'Shelly' and your goal is to assist the user in the best way possible with thier studying.",
+        description="You are an AI called 'GroqRAG' and your task is to answer questions using the provided information",
         instructions=[
-            "Given a user query, first ALWAYS search your knowledge base using the `search_knowledge_base` tool to see if you have relevant information.",
-            "If you dont find relevant information in your knowledge base, use the `duckduckgo_search` tool to search the internet.",
-            "If you need to reference the chat history, use the `get_chat_history` tool.",
-            "If the users question is unclear, ask clarifying questions to get more information.",
-            "Carefully read the information you have gathered and provide a clear and concise answer to the user.",
+            "When a user asks a question, you will be provided with information about the question.",
+            "Carefully read this information and provide a clear and concise answer to the user.",
             "Do not use phrases like 'based on my knowledge' or 'depending on the information'.",
+            "The user is a ${user_grade}, ensure your response tailors to the user's level of understanding",
+            "Whenever a user says the word - generate; Do the following:",
+            "Generate summaries based on your knowledge based",
+            "Create flash cards based on your knowledge base",
+            "Create questions with answers based on your knowledge base",
+            "Do not say anything like - Let me know if you'd like me to generate more content!"
         ],
-        # Show tool calls in the chat
-        show_tool_calls=True,
-        # This setting gives the LLM a tool to search the knowledge base for information
-        search_knowledge=True,
-        # This setting gives the LLM a tool to get chat history
-        read_chat_history=True,
-        tools=[DuckDuckGo()],
+        # This setting adds references from the knowledge_base to the user prompt
+        add_references_to_prompt=True,
         # This setting tells the LLM to format messages in markdown
         markdown=True,
-        # Adds chat history to messages
+        # This setting adds chat history to the messages
         add_chat_history_to_messages=True,
+        # This setting adds 4 previous messages from chat history to the messages
+        num_history_messages=4,
         add_datetime_to_instructions=True,
         debug_mode=debug_mode,
     )
